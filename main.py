4ada514da3b55cf1c288499c7edf0a6782a2e8e8
Kaz95 <smurfrektshrekt@gmail.com>
2022-03-04 17:54:41 -0600

    - Refactored 'main' function

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ def toggle_schedule(schedule):
     return schedule
 
 
+# TODO: Consider using date object instead of datetime
 class Date(datetime.datetime):
 
     def __new__(cls, year, month, day, hour, minute, second, microsecond, tzinfo, infusion=None):
@@ -100,16 +101,16 @@ def randomize_bleed_episode(start, maximum_days):
     return Bepisode(bleed_start, location, duration)
 
 
-def couple_bleeds_to_dates(bepisodes_list, log):
+def couple_bleeds_to_dates(bepisodes_list, some_log):
     for bepisode in bepisodes_list:
         for day in bepisode.dates:
             try:
-                date_to_tag_index = log.index(day)
-                date_to_tag = log[date_to_tag_index]
+                date_to_tag_index = some_log.index(day)
+                date_to_tag = some_log[date_to_tag_index]
                 date_to_tag.bleeds.append(bepisode.location)
             except ValueError:
                 print('Bleed projected passed end of window, probably.')
-    return log
+    return some_log
 
 
 def random_all_bleed_episodes(amount, start, maximum_days, bepi_list):
@@ -128,18 +129,18 @@ def randomize_time_stamp(start_hr, end_hr):
     return datetime.time(hour=rand_hr, minute=rand_minute)
 
 
-def add_infusions_to_log(log, cur_proph_schedule):
+def add_infusions_to_log(some_log, cur_proph_schedule):
     doses = 12
     new_list = []
-    for _ in log:
+    for _ in some_log:
         if doses > 1:
             new_list.append(_)
             if _.bleeds:
-                cur_min_one = log.index(_) - 1
-                cur_min_two = log.index(_) - 2
+                cur_min_one = some_log.index(_) - 1
+                cur_min_two = some_log.index(_) - 2
                 if cur_min_one and cur_min_two >= 0:
                     try:
-                        if log[cur_min_one].infused and log[cur_min_two].infused:
+                        if some_log[cur_min_one].infused and some_log[cur_min_two].infused:
                             continue
                     except ValueError:
                         print('index was out of range when checking prev two days, but was handled')
@@ -193,7 +194,7 @@ def get_manual_bleeds():
     return bepi_list
 
 
-if __name__ == '__main__':
+def fill_log():
     start_date = get_start_date()
     fdate = start_date.strftime('%A - %m/%d/%Y')\
 
@@ -206,21 +207,33 @@ if __name__ == '__main__':
     log_with_bleeds = couple_bleeds_to_dates(bep_list, blank_log)
 
     full_log = add_infusions_to_log(log_with_bleeds, cur_prophey_schedule)
-    end_log = []
-    for _ in full_log:
+    return full_log
+
+
+def sift_log(some_log):
+    sifted_log = []
+    for _ in some_log:
         if _.bleeds or _.infused:
-            end_log.append(_)
-    for _ in end_log:
+            sifted_log.append(_)
+    for _ in sifted_log:
         if _.bleeds:
             print(f'{_} - {_.infused} - {_.bleeds}')
         else:
             print(f'{_} - {_.infused} - Prophey')
+    return sifted_log
 
-    end_log_start = end_log[0]
-    end_log_start = end_log_start.strftime('%m-%d-%Y')
-    end_log_end = end_log[-1]
-    end_log_end = end_log_end.strftime('%m-%d-%Y')
-    with open(f'{end_log_start} - {end_log_end}.csv', 'x', newline='') as csv_log:
+
+def make_csv_title(some_log):
+    start_date = some_log[0]
+    start_date_string = start_date.strftime('%m-%d-%Y')
+    end_date = some_log[-1]
+    end_date_string = end_date.strftime('%m-%d-%Y')
+    return start_date_string, end_date_string
+
+
+def output_to_csv(some_log):
+    csv_title = make_csv_title(some_log)
+    with open(f'{csv_title[0]} - {csv_title[1]}.csv', 'x', newline='') as csv_log:
         log_writer = csv.writer(csv_log)
         log_writer.writerow(['Date', 'Infused', 'time-stamp', 'Reason'])
         for _ in end_log:
@@ -231,3 +244,11 @@ if __name__ == '__main__':
                     log_writer.writerow([_, 'No', 'N/A', _.bleeds])
             else:
                 log_writer.writerow([_, 'Yes', _.time_stamp, 'Prophylaxis'])
+
+
+if __name__ == '__main__':
+    log = fill_log()
+    end_log = sift_log(log)
+    output_to_csv(end_log)
+
+    
