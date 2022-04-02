0f98f7ef98832ae03bffba139bf9df19c8e02a59
kazact <kazact95@gmail.com>
2022-04-02 07:51:19 -0500

    Added first example testcase

---
+++ test.py
@@ -0,0 +1,9 @@
+import unittest
+
+
+class TestPureFunctions(unittest.TestCase):
+    def test_get_date(self):
+        # Check isinstance
+        # Check attribute values
+        # Check bad inputs. Will need to update get_date()
+        pass
