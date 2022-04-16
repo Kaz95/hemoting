d7c1d422f556a38def72191cf46a768430959db7
Kaz95 <smurfrektshrekt@gmail.com>
2022-04-15 20:21:46 -0500

    added prototype CLI interface

--- src/interface.py
+++ src/interface.py
@@ -1,1 +1,19 @@
-"""Module for implementing CLI"""
+"""Module for implementing CLI and GUI"""
+import core
+import settings
+
+
+def run_cli():
+    setting_handler = settings.initialize_settings()
+    while True:
+        command = input('$ ')
+        match command.split():
+            case ['quit' | 'q' | 'Q']:  # Brackets are needed to specify sequence, could also be () or a single ,
+                print('quitting...')
+                break
+            case ['run' | 'go']:
+                log = core.generate_log(setting_handler)
+                core.print_log(log)
+                core.output_log_to_csv(log)
+            case _:
+                print(f'Command {command!r} not found')  # !r flag is used to give output quotes around command.
