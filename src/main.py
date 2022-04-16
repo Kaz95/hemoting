30cb73ea8fa0a3d4554e1e454c3a9ff386f1b92a
Kaz95 <smurfrektshrekt@gmail.com>
2022-04-16 13:53:26 -0500

    fixed bug where settings weren't being saved at any point during program execution, when run via CLI

--- src/main.py
+++ src/main.py
@@ -4,3 +4,4 @@ import settings
 if __name__ == '__main__':
     setting_handler = settings.initialize_settings()
     interface.run_cli(setting_handler)
+    settings.save_settings(setting_handler)  # TODO: Settings will not be saved correctly if program exits early.
