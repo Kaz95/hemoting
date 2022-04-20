b54811e1a97d29cb112d8c19260ade3db984b2a1
kazact <kazact95@gmail.com>
2022-04-19 21:39:29 -0500

    added comments explaining my plans and thoughts on how the app module will be used

--- src/app.py
+++ src/app.py
@@ -9,5 +9,15 @@ class CommandSetHandler:
     pass
 
 
+# This could easily be a 'main' function. A class allows for namespacing, holding state(settings), and the coupling
+# of logic to the aforementioned state. Consider if this is the best solution later.
 class App:
+    pass
+
+
+if __name__ == '__main__':
+    # Initialize Handlers w/ chosen interface and command set
+    # run interface via handler
+    # Command set is responsible for handling commands.
+    # I can change command sets on the fly via handler
     pass
