b7fa1aca2e7d51058d79d8c453862435429bf05d
kazact <kazact95@gmail.com>
2022-04-19 19:40:59 -0500

    rip oldge stuff

--- src/main.py
+++
@@ -1,7 +0,0 @@
-import interface
-import settings
-
-if __name__ == '__main__':
-    setting_handler = settings.initialize_settings()
-    interface.run_cli(setting_handler)
-    settings.save_settings(setting_handler)  # TODO: Settings will not be saved correctly if program exits early.
